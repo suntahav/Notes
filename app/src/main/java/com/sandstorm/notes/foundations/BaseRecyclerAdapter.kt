a65c814caad6e71615a71eb6b6f61aae9429cca7
Sandeep <vijeta013@gmail.com>
2019-12-14 19:23:22 +0530

    DiffUtil and Extension Functions

--- app/src/main/java/com/sandstorm/notes/foundations/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/sandstorm/notes/foundations/BaseRecyclerAdapter.kt
@@ -1,15 +1,17 @@
 package com.sandstorm.notes.foundations
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseRecyclerAdapter<T>(
     protected val masterlist: MutableList<T> = mutableListOf()
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     fun updateList(list: List<T>) {
+        val result = DiffUtil.calculateDiff(DiffUtilCallBackImpl(masterlist,list))
         masterlist.clear()
         masterlist.addAll(list)
-        notifyDataSetChanged()
+        result.dispatchUpdatesTo(this)
     }
 
     override fun getItemCount(): Int {
@@ -39,5 +41,16 @@ abstract class BaseRecyclerAdapter<T>(
     companion object {
         const val TYPE_ADD_BUTTON = 0
         const val TYPE_INFO = 1
+    }
+
+    class DiffUtilCallBackImpl<T>(val oldList: List<T>, val newList: List<T>) : DiffUtil.Callback(){
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition]==newList[newItemPosition]
+
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition]==newList[newItemPosition]
+
     }
 }
